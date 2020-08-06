a057601252bcc44159208249ed7f83a7ee441293
Willem de Bruijn <willemb@google.com>
2020-08-06 07:23:58 -0400

    headers: update license info
    Add or expand GPL-2.0 license at the top of source files.
    
    This does not change the license, which was always clearly recorded
    in the LICENSE file.
    
    Just clarifying, in case anyone would copy a single file.
    Generated using github.com/mbrukman/autogen
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>

--- tests/toeplitz.sh
+++ tests/toeplitz.sh
@@ -25,6 +25,23 @@
 # e.g., for i in `seq 10`; do echo "ping $i" | nc -w 0 -6 -u ${HOST} 8000; sleep 0.02; done
 #
 # see/modify these config options for protocol details:
+#
+#
+# Copyright (C) 2020 Google LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 IPVER=-6
 PROTO=-u
