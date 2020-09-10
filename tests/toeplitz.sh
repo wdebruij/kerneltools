5668fcc992efd447a421be11f7b8201702c29cbd
Tanner Love <tannerlove@google.com>
2020-09-10 12:21:18 -0400

    Add option to toeplitz test to verify RPS functionality
    Add the option '-r <rps_bitmap>' to toeplitz.c, which verifies that
    frames are handled by CPUs according to the RPS specification.
    
    Add the option '-rps <rps_bitmap>' to toeplitz.sh as a hook for
    specifying this test. This test requires that rps_bitmap is applied
    to all rx queues (i.e. all /sys/class/net/${DEV}/queues/rx-*/rps_cpus)
    so that RSS is rendered effectively immaterial; if this condition is
    not met, then toeplitz.sh does not proceed with the RPS test.
    
    The test verifies that the CPU that processes each frame is chosen by
    using hash % <number of "on" CPUs in the bitmap> as an index into the
    bitmap, in the same way as get_rps_cpu.
    
    Users should specify either '-C <cpu_list>' or '-r <rps_bitmap>' to
    test either RSS or RPS, respectively.
    
    Signed-off-by: Tanner Love <tannerlove@google.com>

--- tests/toeplitz.sh
+++ tests/toeplitz.sh
@@ -17,10 +17,13 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 #
-# extended toeplitz test: test rxhash plus rss mapping from rxhash to rx queue.
+# extended toeplitz test: test rxhash plus, optionally, either (1) rss mapping
+# from rxhash to rx queue ('-irqprefix <irq_prefix>') or (2) rps mapping from
+# rxhash to cpu ('-rps <rps_map>')
 #
 # invoke as:
-# ./toeplitz.sh -dev <dev> [-u|-t] [-4|-6] [-irqprefix <irq_prefix>]
+# ./toeplitz.sh -dev <dev> [-u|-t] [-4|-6] \
+#   [(-irqprefix <irq_prefix>)|(-rps <rps_map>)]
 #
 # then generate traffic from another host to this host
 # e.g., for i in `seq 10`; do echo "ping $i" | nc -w 0 -6 -u ${HOST} 8000; sleep 0.02; done
@@ -32,6 +35,7 @@ PROTO=-u
 DPORT=8000
 DEV=""
 IRQ_PREFIX=""
+RPS_MAP=""
 
 set -e
 
@@ -66,10 +70,22 @@ get_rx_irq_cpus() {
 	echo "${CPUS}"
 }
 
-# RPS/RFS must be disabled because they move packets between cpus,
-# which breaks the PACKET_FANOUT_CPU identification of RSS decisions.
-check_rpsrfs_disabled() {
-	local -r RFS_FILE="/proc/sys/net/core/rps_sock_flow_entries"
+check_rps_configs() {
+	local -r qdir=/sys/class/net/"${DEV}"/queues
+	local -r rps_map_zero="$(<${qdir}/rx-0/rps_cpus)"
+	for rps_cpus_file in "${qdir}"/rx-*/rps_cpus
+	do
+		rps_map_curr="$(<${rps_cpus_file})"
+		if [[ "${rps_map_curr}" != "${rps_map_zero}" ]]; then
+			echo "RPS bitmaps for all rxqs must match for RPS test"
+			echo "${qdir}/rx-0/rps_cpus contains: ${rps_map_zero}"
+			echo "${rps_cpus_file} contains: ${rps_map_curr}"
+			exit 1
+		fi
+	done
+}
+
+check_rps_disabled() {
 	local -r RPS_FILE="/sys/class/net/${DEV}/queues/rx-0/rps_cpus"
 
 	local -r RPS_CONF=$(cat "${RPS_FILE}" | tr -d '0,')
@@ -77,6 +93,10 @@ check_rpsrfs_disabled() {
 		echo "RPS must be disabled (${RPS_FILE}: ${RPS_CONF} != 0)"
 		exit 1
 	fi
+}
+
+check_rfs_disabled() {
+	local -r RFS_FILE="/proc/sys/net/core/rps_sock_flow_entries"
 
 	if [[ -f "${RFS_FILE}" ]]; then
 		local -r RFS_CONF=$(< "${RFS_FILE}")
@@ -94,8 +114,9 @@ die() {
 
 show_usage_and_die() {
 	echo "Usage: $0 -dev <dev> [-u|-t] [-4|-6] \\"
-	echo "  [-irqprefix <irq_prefix>]"
-	die "ex: $0 -dev eth0 -u -6 -irqprefix eth0-rx-"
+	echo "  [(-irqprefix <irq_prefix>)|(-rps <rps_map>)]"
+	echo "ex: $0 -dev eth0 -u -6 -irqprefix eth0-rx-"
+	die "ex: $0 -dev eth0 -t -4 -rps f"
 }
 
 check_nic_rxhash_enabled() {
@@ -116,6 +137,9 @@ parse_opts() {
 			PROTO="$1"
 		elif [[ "$1" = "-4" || "$1" = "-6" ]]; then
 			IPVER="$1"
+		elif [[ "$1" = "-rps" ]]; then
+			shift
+			RPS_MAP="$1"
 		else
 			show_usage_and_die
 		fi
@@ -134,10 +158,22 @@ RSS_KEY=$(</proc/sys/net/core/netdev_rss_key)
 check_nic_rxhash_enabled
 
 if [[ ! -z "${IRQ_PREFIX}" ]]; then
-	check_rpsrfs_disabled
+	# Test RSS: rxhash % num_rxq maps to expected rxqueue.
+	# RPS/RFS must be disabled because they move packets between cpus,
+	# which breaks the PACKET_FANOUT_CPU identification of RSS decisions.
+	check_rps_disabled
+	check_rfs_disabled
 	./toeplitz "${IPVER}" "${PROTO}" -d "${DPORT}" -i "${DEV}" \
 	  -k "${RSS_KEY}" -T 1000 -C "$(get_rx_irq_cpus)" -s -v
+elif [[ ! -z "${RPS_MAP}" ]]; then
+	# Test RPS: rxhash % num_cpu maps to expected cpu in rps_map.
+	# Requires that all rxqueues have the same rps_map.
+	check_rps_configs
+	check_rfs_disabled
+	./toeplitz "${IPVER}" "${PROTO}" -d "${DPORT}" -i "${DEV}" \
+	  -k "${RSS_KEY}" -T 1000 -r "0x${RPS_MAP}" -s -v
 else
+	# Test rxhash: read rxhash only.
 	./toeplitz "${IPVER}" "${PROTO}" -d "${DPORT}" -i "${DEV}" \
 	  -k "${RSS_KEY}" -T 1000 -s -v
 fi
